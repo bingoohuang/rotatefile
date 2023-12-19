cdef46b1362df254b42022b42368273f58ee9b1e
huangjinbing <bingoo.huang@gmail.com>
2023-12-19 17:49:50 +0800

    Initial commit

---
+++ example_test.go
@@ -0,0 +1,17 @@
+package rotatefile
+
+import (
+	"log"
+)
+
+// To use lumberjack with the standard library's log package, just pass it into
+// the SetOutput function when your application starts.
+func Example() {
+	log.SetOutput(&Logger{
+		Filename:   "/var/log/myapp/foo.log",
+		MaxSize:    500, // megabytes
+		MaxBackups: 3,
+		MaxDays:    28,   // days
+		Compress:   true, // disabled by default
+	})
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module gopkg.in/bingoohuang/rotatefile
+
+go 1.13
